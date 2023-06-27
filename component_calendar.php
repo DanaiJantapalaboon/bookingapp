0b29f9137655f95b90b2db02a0a564f57cad865c
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2023-06-28 01:33:37 +0700

    Add files via upload

--- component_calendar.php
+++ component_calendar.php
@@ -1,7 +1,7 @@
 <div class="container" id="contentBox">
     <div class="text-end">
         <p><b>Welcome Anonymous, Please <a href="#" data-bs-toggle="modal" data-bs-target="#userLogin">Login</a> First!</b><br>
-            Cleanroom Booking System - Version 1.00</p>
+        BookingApp System - Version 1.00</p>
     </div>
 
     <h3 class="text-center mt-4">Scheduling Calendar</h3>
